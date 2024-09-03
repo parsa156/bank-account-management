30648b75a7c9f324cc6a7f26e0957d57b933d209
parsa <parsahashemian15603@gmail.com>
2024-09-03 12:37:06 +0330

    ezafe kardan class bank dar model account

--- accounts/models.py
+++ accounts/models.py
@@ -9,3 +9,10 @@ class BankAccount(models.Model):
     
     def __str__(self):
         return f"{self.bank_name} - {self.account_number}"
+class Bank(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.name    
