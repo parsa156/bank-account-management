6c2a204ff45e61f6679fcd2cd92a8651ecab1d2c
parsa <parsahashemian15603@gmail.com>
2024-09-06 00:11:01 +0330

    code viewset va url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BankAccountViewSet
+from .views import BankAccountViewSet , BankViewSet
 
 router = DefaultRouter()
 router.register(r'accounts', BankAccountViewSet)
+router.register(r'banks', BankViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 
 from rest_framework import viewsets
-from .models import BankAccount
-from .serializers import BankAccountSerializer
+from .models import BankAccount , Bank
+from .serializers import BankAccountSerializer , BankSerializer
 
 class BankAccountViewSet(viewsets.ModelViewSet):
     queryset = BankAccount.objects.all()
     serializer_class = BankAccountSerializer
+class BankViewSet(viewsets.ModelViewSet):
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializer
