0a1c991701f7812e6ae9e23383472c1dc26e7950
parsa <parsahashemian15603@gmail.com>
2024-09-06 00:22:00 +0330

    debug

--- accounts/models.py
+++ accounts/models.py
@@ -3,13 +3,13 @@ from customers.models import Customer
 
 class Bank(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=15)
+    phone_number = models.CharField(max_length=15 , blank=True , null=True)
 
     def __str__(self):
         return self.name 
 class BankAccount(models.Model):
     customer = models.ForeignKey(Customer, related_name='accounts', on_delete=models.CASCADE)
-    bank = models.ForeignKey(Bank, related_name='accounts', on_delete=models.CASCADE)
+    bank = models.ForeignKey(Bank, related_name='accounts', on_delete=models.CASCADE, default=1)
     bank_name = models.CharField(max_length=100)
     account_number = models.CharField(max_length=30, unique=True)
     balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
