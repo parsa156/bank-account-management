76d841c1c97a1ce4894f95f327574d1cc7e9af3c
parsa <parsahashemian15603@gmail.com>
2024-09-18 12:40:46 +0330

    make sure that some field will be uneditable

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework import viewsets
 from .models import BankAccount 
 from .serializers import BankAccountSerializer 
--- customers/models.py
+++ customers/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.core.validators import MinLengthValidator
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100, editable=False)
+    last_name = models.CharField(max_length=100, editable=False)
+    email = models.EmailField(unique=True, editable=False)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-    password = models.CharField(max_length=20,validators=[MinLengthValidator(8)])
     
     def __str__(self):
          return f"{self.first_name} {self.last_name}"
