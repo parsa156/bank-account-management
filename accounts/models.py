186d332e5917830bf08072b768b071e7460d4068
parsa <parsahashemian15603@gmail.com>
2024-09-18 12:40:46 +0330

    make sure that some field will be uneditable

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,9 @@ from customers.models import Customer
 from bank.models import Bank
 
 class BankAccount(models.Model):
-    customer = models.ForeignKey(Customer, related_name='accounts', on_delete=models.CASCADE)
-    bank = models.ForeignKey(Bank, related_name='accounts', on_delete=models.CASCADE)
-    account_number = models.CharField(max_length=30, unique=True)
+    customer = models.ForeignKey(Customer, related_name='accounts', editable=False, on_delete=models.CASCADE)
+    bank = models.ForeignKey(Bank, related_name='accounts', editable=False, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=30, unique=True, editable=False)
     balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     
     def __str__(self):
