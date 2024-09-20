f0beb02833761e1228d172ddb1a40d4b736a59fc
parsa <parsahashemian15603@gmail.com>
2024-09-20 13:31:21 +0330

    make the url right

--- authentication/views.py
+++ authentication/views.py
@@ -6,13 +6,13 @@ from rest_framework.permissions import AllowAny
 
 class RegisterView(APIView):
 
-    permission_classes = [AllowAny]  # Allows anyone to register without authentication
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = RegisterSerializer(data=request.data)
         
         if serializer.is_valid():
-            user = serializer.save()  # Save the new user
+            user = serializer.save()  
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
