99a9bbe645eae1c8544a5a9b86a694f270d3305d
parsa <parsahashemian15603@gmail.com>
2024-09-20 13:11:21 +0330

    make views for sign up

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class RegisterView(APIView):
+
+    permission_classes = [AllowAny]  # Allows anyone to register without authentication
+
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()  # Save the new user
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
