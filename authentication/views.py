a0c95015dff125abf3ae3b0f74ed181d0ca1b358
parsa <parsahashemian15603@gmail.com>
2024-09-21 14:41:34 +0330

    view for sign in

--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RegisterSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
+###sign up
 class RegisterView(APIView):
 
     permission_classes = [AllowAny]
@@ -16,3 +20,23 @@ class RegisterView(APIView):
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+###sign in
+class LoginView(TokenObtainPairView):
+    
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        
+        user = authenticate(request, email=email, password=password)
+
+        if user:
+            refresh = RefreshToken.for_user(user)  # Create a refresh token for the user
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),  # Create an access token for the user
+            })
+        else:
+            return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
