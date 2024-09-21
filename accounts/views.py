0a28a1bf4ac5e7ade69bca74f8d772d483f14896
parsa <parsahashemian15603@gmail.com>
2024-09-21 18:57:41 +0330

    urls for sign in

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import BankAccount 
 from .serializers import BankAccountSerializer 
+from rest_framework.permissions import IsAuthenticated
 class BankAccountViewSet(viewsets.ModelViewSet):
     queryset = BankAccount.objects.all()
     serializer_class = BankAccountSerializer
+    permission_classes = [IsAuthenticated]
--- bank/views.py
+++ bank/views.py
@@ -1,8 +1,10 @@
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Bank
 from .serializers import BankSerializer
 
 class BankViewSet(viewsets.ModelViewSet):
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
+    permission_classes = [IsAuthenticated]
