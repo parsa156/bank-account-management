673670801105b011a9a4996e6ab24a6fba6f4c9f
parsa <parsahashemian15603@gmail.com>
2024-09-21 18:57:41 +0330

    urls for sign in

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import Customer
 from .serializers import CustomerSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
