c9fe856fc59388a8dee3f7a87202685cd9c208a1
parsa <parsahashemian15603@gmail.com>
2024-09-29 17:32:09 +0330

    change view and urls for acconts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,8 @@
-
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import BankAccountViewSet 
-
-router = DefaultRouter()
-router.register(r'accounts', BankAccountViewSet)
+from django.urls import path
+from .views import BankAccountListView, BankAccountCreateView, BankAccountDetailView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', BankAccountListView.as_view(), name='bankaccount-list'),             # GET all accounts
+    path('create/', BankAccountCreateView.as_view(), name='bankaccount-create'),    # POST to create an account
+    path('ccounts/<int:pk>/', BankAccountDetailView.as_view(), name='bankaccount-detail'),   # GET, PUT, DELETE an account
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,42 @@
-from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 from .models import BankAccount
-from .serializers import BankAccountSerializer, BankAccountPostSerializer
+from .serializers import BankAccountSerializer , BankAccountPostSerializer
 
-class BankAccountViewSet(viewsets.ModelViewSet):
-    queryset = BankAccount.objects.all()
+# List all Bank Accounts (GET)
+class BankAccountListView(APIView):
+    def get(self, request):
+        accounts = BankAccount.objects.all()
+        serializer = BankAccountSerializer(accounts, many=True)
+        return Response(serializer.data)
 
-    def get_serializer_class(self):
-        if self.request.method == 'POST':
-            return BankAccountPostSerializer
-        return BankAccountSerializer
+# Create a Bank Account (POST)
+class BankAccountCreateView(APIView):
+    def post(self, request):
+        serializer = BankAccountPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Retrieve, Update, Delete a Bank Account
+class BankAccountDetailView(APIView):
+    def get(self, request, pk):
+        account = get_object_or_404(BankAccount, pk=pk)
+        serializer = BankAccountSerializer(account)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        account = get_object_or_404(BankAccount, pk=pk)
+        serializer = BankAccountSerializer(account, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        account = get_object_or_404(BankAccount, pk=pk)
+        account.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
