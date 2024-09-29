13d0a5b6c0cd15fad4d2969234612ced94f73399
parsa <parsahashemian15603@gmail.com>
2024-09-29 12:28:17 +0330

    change serializers and view(only post the other like get come soon ...)

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,21 +1,24 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-User = get_user_model()  
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
-        fields = ('email', 'password', 'first_name', 'last_name') 
+        fields = ('email', 'password', 'first_name', 'last_name')
 
-    def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
-        )
-        user.set_password(validated_data['password'])  
-        user.save()  
-        return user
+class RegisterPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'first_name', 'last_name')
+
+    def get_fields(self):
+        fields = super().get_fields()
+        request_method = self.context['request'].method
+        if request_method == 'POST':
+            fields['email'].read_only = False
+        else:
+            fields['email'].read_only = True
+        return fields
+
--- authentication/views.py
+++ authentication/views.py
@@ -1,42 +1,18 @@
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import RegisterSerializer
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer, RegisterPostSerializer
 
-###sign up
 class RegisterView(APIView):
 
-    permission_classes = [AllowAny]
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return RegisterPostSerializer
+        return RegisterSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = RegisterSerializer(data=request.data)
-        
+        serializer = self.get_serializer_class()(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()  
+            serializer.save()
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
-        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-###sign in
-class LoginView(TokenObtainPairView):
-    
-    def post(self, request, *args, **kwargs):
-        email = request.data.get('email')
-        password = request.data.get('password')
-
-        
-        user = authenticate(request, email=email, password=password)
-
-        if user:
-            refresh = RefreshToken.for_user(user)  # Create a refresh token for the user
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),  # Create an access token for the user
-            })
-        else:
-            return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
