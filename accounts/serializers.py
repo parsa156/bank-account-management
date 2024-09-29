7beacd9ae3666a45c17c12262e316350289ad31b
parsa <parsahashemian15603@gmail.com>
2024-09-29 12:03:14 +0330

    change serializers and view for accounts

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,26 @@
-
 from rest_framework import serializers
-from .models import BankAccount 
+from .models import BankAccount
+
 class BankAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = BankAccount
         fields = '__all__'
+
+class BankAccountPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BankAccount
+        fields = '__all__'
+
+    def get_fields(self):
+        fields = super().get_fields()
+        request_method = self.context['request'].method
+        if request_method == 'POST':
+            fields['customer'].read_only = False  
+            fields['bank'].read_only = False  
+            fields['account_number'].read_only = False  
+           
+        else:
+            fields['customer'].read_only = True  
+            fields['bank'].read_only = True  
+            fields['account_number'].read_only = True
+        return fields
