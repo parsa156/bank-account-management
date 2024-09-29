5366e9074dbe4bd96d2660afc8161990e9f0f996
parsa <parsahashemian15603@gmail.com>
2024-09-29 11:50:07 +0330

    change serializers and view customers

--- customers/serializers.py
+++ customers/serializers.py
@@ -5,3 +5,23 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+
+class CustomerPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+    def get_fields(self):
+        fields = super().get_fields()
+        request_method = self.context['request'].method
+        if request_method == 'POST':
+            fields['email'].read_only = False  
+            fields['first_name'].read_only = False  
+            fields['last_name'].read_only = False  
+
+        else:
+            fields['email'].read_only = True
+            fields['first_name'].read_only = True  
+            fields['last_name'].read_only = True  
+
+        return fields
--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from .models import Customer
-from .serializers import CustomerSerializer
-from rest_framework.permissions import IsAuthenticated
+from .serializers import CustomerSerializer, CustomerPostSerializer
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CustomerPostSerializer
+        return CustomerSerializer
