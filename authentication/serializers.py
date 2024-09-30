f76a7f1dfc948aa1a01e1031c025b2a4288f1d4d
parsa <parsahashemian15603@gmail.com>
2024-09-30 16:48:32 +0330

    fix bug (Serializers): about edit put or post authentication and migrate

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -8,17 +8,15 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ('email', 'password', 'first_name', 'last_name')
 
-class RegisterPostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('email', 'password', 'first_name', 'last_name')
+    def update(self, instance, validated_data):
+        if 'email' in validated_data and instance.email != validated_data['email']:
+            raise serializers.ValidationError({"email": "You cannot change the email after registration."})
 
-    def get_fields(self):
-        fields = super().get_fields()
-        request_method = self.context['request'].method
-        if request_method == 'POST':
-            fields['email'].read_only = False
-        else:
-            fields['email'].read_only = True
-        return fields
+        if 'first_name' in validated_data and instance.first_name != validated_data['first_name']:
+            raise serializers.ValidationError({"first_name": "You cannot change the first name after registration."})
+
+        if 'last_name' in validated_data and instance.last_name != validated_data['last_name']:
+            raise serializers.ValidationError({"last_name": "You cannot change the last name after registration."})
+
+        return super().update(instance, validated_data)
 
