ec1e81fa6775802662cffb5175514b8bed05de91
parsa <parsahashemian15603@gmail.com>
2024-10-04 11:07:19 +0330

    account number format and account generate

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from customers.models import Customer
 from bank.models import Bank
+import random
 
 class BankAccount(models.Model):
     customer = models.ForeignKey(Customer, related_name='accounts', on_delete=models.CASCADE, blank=False, null=False)
@@ -12,4 +13,23 @@ class BankAccount(models.Model):
     
     def __str__(self):
         return f"{self.bank_name} - {self.account_number}"
+    
+    
+    def save(self, *args, **kwargs):
+        if not self.account_number:
+            self.account_number = self.generate_account_number()
+        super().save(*args, **kwargs)
+
+    
+    def generate_account_number(self):
+        # Get format from the bank's account_number_format
+        format_template = self.bank.account_number_format
+
+        account_number = ''
+        for char in format_template:
+            if char == 'x':  # Replace 'x' with a random digit
+                account_number += str(random.randint(0, 9))
+            else:
+                account_number += char
+        return account_number
    
